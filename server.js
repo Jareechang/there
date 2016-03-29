e51a7f5a8197db68c198cf0c9d8b599bd2cbda30
Jerry Chang <therealjerrycan@gmail.com>
2016-03-28 20:07:52 -0700

    setting up server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+
+/**
+ * configuration ––––––––––––––––––––––––––––––––––––––
+ */
+
+app.use(morgan('dev'));  // handling loging in conosle
+app.use(cookieParser()); // handling cookies
+app.use(bodyParser());   // handling info from html forms
+
+app.set('view engine', 'ejs'); // use EJS for templating
+
+
