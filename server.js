8998e69b51875a7be723397ebc0f7224ceddc8e9
Jerry Chang <therealjerrycan@gmail.com>
2016-03-28 20:28:25 -0700

    updated the root url routing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
+var passport = require('passport');
 var flash = require('connect-flash');
 
 var morgan = require('morgan');
@@ -15,6 +16,7 @@ var env = require('./config/env/all.js') || {};
 /**
  * configuration ––––––––––––––––––––––––––––––––––––––
  */
+// require('./config/passport')(passport);
 
 app.use(morgan('dev'));  // handling loging in conosle
 app.use(cookieParser()); // handling cookies
@@ -22,5 +24,18 @@ app.use(bodyParser());   // handling info from html forms
 
 app.set('view engine', 'ejs'); // use EJS for templating
 
-console.log(env.authLocal.secret);
+app.use(session({ secret: env.authLocal.secret }));
+app.use(passport.initialize());
+app.use(passport.session());  // persistent login 
+app.use(flash());
+
+/**
+ * routes ––––––––––––––––––––––––––––––––––––––
+ */
+
+require('./app/routes.js')(app, passport); // Load routes with app && loaded passport as depedencies
+
+app.listen(port);
+
+console.log('stuff is happen on ' + port);
 
