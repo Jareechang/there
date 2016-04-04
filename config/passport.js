a0fa5f32e3bedef3e23008cd588ac7a3425b26b7
Jerry Chang <therealjerrycan@gmail.com>
2016-04-03 20:50:17 -0700

    added passport config for local-signup

--- config/passport.js
+++ config/passport.js
@@ -1,20 +1,63 @@
 var localStrategy = require('passport').Strategy;
 var User = require('../models').User;
 
-/** 
- *  *** TEST CODE **** 
- * 
- * */
-//User.create({ firstName: "em", lastName: "Cool", password: "dogs", passwordConfirmation: "dogs", email: "em@world.com"})
-//User.findById(19)
-    //.then(function(user){
-        //console.log("password is ... " + user.validPassword("dogs"));
-    //})
-
 module.exports = function(passport) {
 
     passport.serializeUser(function(user, done){
         done(null, cb);
     })
+
+    passport.deserializeUser(function(id, done){
+        User.findById(id)
+            .then(function(user){
+                done(err,user);
+            })
+    })
+
+    passport.user('local-signup', new localStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+        
+    }, function(req, firstName, lastName, email, password,passwordConfirmation, done){
+
+        process.nextTick(function(){
+
+            var userEmail = email;
+
+            User.findOrCreate({
+                where: {
+                    email: userEmail 
+                }
+            })
+            .then(function(user){
+                if(user) {
+                    return done(null, false, req.flash('signUpMessage', 'Email is already taken'));
+                }
+                else  {
+
+                    var newUser = User.build(
+                                   firstName: firstName,
+                                    lastName: lastName
+                                       email: email,
+                        passwordConfirmation: passwordConfirmation,
+                                    password: password
+                    );
+
+                    newUser.save()
+                        .then(function(user){
+                            return done(null, user)
+                        })
+                        .error(function(err){
+                            throw err;
+                        });
+                }
+            })
+            .error(function(err){
+                return done(err);
+            })
+        })
+
+    }))
 }
 
