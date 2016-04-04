69785bf47d070b5f51479b0c384a1adc626199af
Jerry Chang <therealjerrycan@gmail.com>
2016-04-03 20:21:11 -0700

    commented out test code in config/passport

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,15 @@
 var localStrategy = require('passport').Strategy;
 var User = require('../models').User;
 
+/** 
+ *  *** TEST CODE **** 
+ * 
+ * */
 //User.create({ firstName: "em", lastName: "Cool", password: "dogs", passwordConfirmation: "dogs", email: "em@world.com"})
-User.findById(19)
-    .then(function(user){
-        console.log("password is ... " + user.validPassword("dogs"));
-    })
+//User.findById(19)
+    //.then(function(user){
+        //console.log("password is ... " + user.validPassword("dogs"));
+    //})
 
 module.exports = function(passport) {
 
