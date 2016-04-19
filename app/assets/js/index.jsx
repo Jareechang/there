c6c3c819f41046ad79a4a4cf7a5781cc13544d1b
Jerry Chang <therealjerrycan@gmail.com>
2016-04-18 21:04:55 -0700

    added webpack-dev-server to the workflow

--- app/assets/js/index.jsx
+++ app/assets/js/index.jsx
@@ -1,29 +1,62 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+import { Modal, Button } from 'react-bootstrap';
+
+/**
+ *  Create Modal Instance
+ */
+
+const modalInstance = ( 
+    <div className="static-modal">
+        <Modal.Dialog>
+
+            <Modal.Header>
+                 <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                One fine body...
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button>Close</Button>
+                <Button bsStyle="primary">Save changes</Button>
+            </Modal.Footer>
+
+        </Modal.Dialog>
+
+    </div>
+);
+
 var LoginModal = React.createClass({
 
-    getInitialState:  function() {
-        return { active: false };
-    }, 
+getInitialState:  function() {
+    return { active: false };
+}, 
 
-    handleClick: function(event) {
-        console.log(`state of login ${this.state.active}`);
-        this.setState({active: !this.state.active});
-    },
+handleClick: function(event) {
+    console.log(`state of login ${this.state.active}`);
+    this.setState({active: !this.state.active});
+},
 
-    render: function() {
-        var link = '#';
-        var text = "Log-in";
-        return(
-            <a href={link} onClick={this.handleClick}>
-                {text}
-            </a>
-        );
-    }
+render: function() {
+    var link = '#';
+    var text = "Log-in";
+    return(
+        <a href={link} onClick={this.handleClick}>
+            {text}
+        </a>
+    );
+}
 })
 
 ReactDOM.render(
     <LoginModal />,
     document.getElementById('login')
+);
+
+ReactDOM.render(
+    modalInstance, 
+    document.getElementById('example')
 );
